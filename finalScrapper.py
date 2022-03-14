3ba24f7513ad1853d12f2d9ed804ca7704503f1a
Saatvik12 <90774068+Saatvik12@users.noreply.github.com>
2022-03-14 18:03:05 +0530

    Add files via upload

---
+++ finalScrapper.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+import requests
+START_URL='https://exoplanets.nasa.gov/discovery/exoplanet-catalog/'
+browser = webdriver.Chrome(executable_path=r'C:\Users\Ananda nilayam\Desktop\C127\chromedriver.exe')
+time.sleep(10)
+def scrap():
+    headers=[ 'NAME', 'LIGHT-YEARS FROM EARTH','PLANET MASS', 'STELLAR MAGNITUDE','DISCOVERY DATE']
+    planet_data=[]
+    for i in range(0,198):
+        while True:
+            time.sleep(2)
+            soup=BeautifulSoup(browser.page_source, 'html.parser')
+            current_page_num=int(soup.find_all('input', attrs={'class','page_num'})[0].get('value'))
+            if current_page_num<i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+            elif current_page_num>i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[1]/a').click()
+            else:
+                break
+        for ul_tag in soup.find_all('ul',attrs={'class','exoplanet'}):
+            litags=ul_tag.find_all('li')
+            temp_list=[]
+            for index,li_tags in enumerate(litags):
+                if index==0:
+                    temp_list.append(li_tags.find_all('a')[0].contents[0])
+                else:
+                    try:
+                       temp_list.append(li_tags.contents[0])
+                    except:
+                       temp_list.append('')
+            hyperlink_li_tag=litags[0]
+            temp_list.append('https://exoplanets.nasa.gov'+hyperlink_li_tag.find_all('a',href=True)[0]['href'])
+            planet_data.append(temp_list)
+            browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+def scrape_more_data(hyperlink):
+    try:
+        page = requests.get(hyperlink)
+        soup = BeautifulSoup(page.content, "html.parser")
+        temp_list = []
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+            for td_tag in td_tags:
+                try:
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+        new_planet_data.append(temp_list)
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
+scrap()
+for index, data in enumerate(planet_data):
+    scrape_more_data(data[5])
+    print(f"{index+1} page done 2")
+final_planet_data = []
+for index, data in enumerate(planet_data):
+    new_planet_data_element = new_planet_data[index]
+    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
+    new_planet_data_element = new_planet_data_element[:7]
+    final_planet_data.append(data + new_planet_data_element)
+with open("final.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(final_planet_data)
